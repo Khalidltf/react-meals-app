6407e52af15f5ef12b27e137c6ce2ef2b4fdb5cd
khalidlotfi <khalidlotfi2005@gmail.com>
2024-04-16 14:53:06 +0100

    Meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,18 +1,35 @@
 import { useGlobalContext } from "../context/context";
+import { LuThumbsUp } from "react-icons/lu";
 
 const Meals = () => {
-  const { meals } = useGlobalContext();
+  const { meals, loading } = useGlobalContext();
+
+  if (loading)
+    return (
+      <section className="section">
+        <h1>Loading...</h1>{" "}
+      </section>
+    );
+
+  if (meals.length < 1)
+    return (
+      <section className="section">
+        <h2>No meals matched your search. Please try again 👀</h2>
+      </section>
+    );
+
   return (
     <section className="section-center">
       {meals.map((meal) => {
         const { idMeal, strMeal: title, strMealThumb: image } = meal;
-
         return (
           <article key={idMeal} className="single-meal">
             <img src={image} className="img" />
             <footer>
               <h5>{title}</h5>
-              <button className="like-btn">click me</button>
+              <button className="like-btn">
+                <LuThumbsUp />
+              </button>
             </footer>
           </article>
         );
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -2,30 +2,36 @@ import { useState, createContext, useContext, useEffect } from "react";
 import axios from "axios";
 
 const AppContext = createContext();
-const allMealsUrl =
-  "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
 // const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 // eslint-disable-next-line react/prop-types
 const AppProvider = ({ children }) => {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchMeals = async (url) => {
+    setLoading(true);
     try {
       const {
         data: { meals },
       } = await axios.get(url);
-      setMeals(meals);
+      meals && setMeals(meals);
       console.log(meals);
     } catch (error) {
       console.log(error.response);
     }
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchMeals(allMealsUrl);
   }, []);
 
-  return <AppContext.Provider value={{meals}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ meals, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
