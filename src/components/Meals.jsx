f6d118a96324ec2af53908a976b07daa5c8ee976
khalidlotfi <khalidlotfi2005@gmail.com>
2024-04-16 10:16:31 +0100

    displaying meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,24 @@
 import { useGlobalContext } from "../context/context";
 
 const Meals = () => {
-  const context = useGlobalContext();
-  return <h1>Hello {context.name}</h1>;
+  const { meals } = useGlobalContext();
+  return (
+    <section className="section-center">
+      {meals.map((meal) => {
+        const { idMeal, strMeal: title, strMealThumb: image } = meal;
+
+        return (
+          <article key={idMeal} className="single-meal">
+            <img src={image} className="img" />
+            <footer>
+              <h5>{title}</h5>
+              <button className="like-btn">click me</button>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Meals;
