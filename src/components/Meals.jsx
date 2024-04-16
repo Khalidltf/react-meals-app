10a614447141a69ebe32e5e9c2f4148885f1bedb
khalidlotfi <khalidlotfi2005@gmail.com>
2024-04-16 07:55:37 +0100

    deleting the console log

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,8 +2,6 @@ import { useGlobalContext } from "../context/context";
 
 const Meals = () => {
   const context = useGlobalContext();
-  console.log(context);
-
   return <h1>Hello {context.name}</h1>;
 };
 
