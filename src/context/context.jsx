dbedca43c0f664d3dfa24695ba205caac71f8373
khalidlotfi <khalidlotfi2005@gmail.com>
2024-04-16 07:56:14 +0100

    fetching data

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,8 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
 // eslint-disable-next-line react/prop-types
 const AppProvider = ({ children }) => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api");
+      const data = await response.json();
+      console.log("Data", data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AppContext.Provider value={{ name: "john D.", age: 23 }}>
       {children}
